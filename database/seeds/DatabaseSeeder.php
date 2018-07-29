cea73a55adaf234102768467d881c4e7ac580c90
kolydart <kolydart@macbeth.gateweb.gr>
2018-07-29 12:24:21 +0300

    disable useless seeds in DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,15 +12,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         
-        $this->call(ArtSeed::class);
+        // $this->call(ArtSeed::class);
         $this->call(ContentPageSeed::class);
         $this->call(PermissionSeed::class);
         $this->call(RoleSeed::class);
-        $this->call(UserSeed::class);
-        $this->call(PaperSeed::class);
-        $this->call(PaperSeedPivot::class);
+        // $this->call(UserSeed::class);
+        // $this->call(PaperSeed::class);
+        // $this->call(PaperSeedPivot::class);
         $this->call(RoleSeedPivot::class);
-        $this->call(UserSeedPivot::class);
+        // $this->call(UserSeedPivot::class);
         $this->call(FirstStageFullDumpSeed::class);
 
     }
